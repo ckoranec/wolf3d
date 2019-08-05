b7454845fff22fcea71ea3115d14e068903906e5
Cameron <cameronlambert98@gmail.com>
2019-08-05 03:11:45 -0700

    rearranged things

--- src/draw.c
+++ src/draw.c
@@ -27,6 +27,16 @@ void		dda(t_mlx *mlx, int start, int end, int x, int type)
 	printf("dda done\n");
 }
 
+void						dda_temp(t_mlx *mlx, int start, int end, int x, int type)
+{
+	int y = start;
+	while (y < end)
+	{
+		image_set_pixel(mlx->image, x, y, colors(type));
+		y++;
+	}
+}
+
 void						draw_column(int x, t_mlx *mlx, double dist, int type)
 {
 	printf("dist %f\n", dist);
@@ -41,84 +51,86 @@ void						draw_column(int x, t_mlx *mlx, double dist, int type)
     if(drawEnd > WIN_HEIGHT - 1)
 		drawEnd = WIN_HEIGHT - 1;
 	if (dist == 0)
-		dda(mlx, 0, WIN_HEIGHT, x, type);
+		dda_temp(mlx, 0, WIN_HEIGHT, x, type);
 	else
-		dda(mlx, drawStart, drawEnd, x, type);
+		dda_temp(mlx, drawStart, drawEnd, x, type);
+}
+
+void						cast(int col, t_mlx *mlx)
+{
+	t_ray		ray;
+    t_player	*player;
+	double		camera;
+	player = &mlx->player;
+	double playerx = player->x;
+	double playery = player->y;
+	double planex = player->camplane_x;
+	double planey = player->camplane_y;
+
+
+	printf("pdir x %f pdir y %f\n", mlx->player.dir_x, mlx->player.dir_y);
+	printf("testing map w %d h %d f_w %f f_h %f\n", mlx->map.width, mlx->map.height, (double)mlx->map.width, (double)mlx->map.height);
+	double mapx = playerx;
+	double mapy = playery;
+
+	camera = 2.0f * col / (double)WIN_WIDTH - 1.0f;
+	ray.dirx = mlx->player.dir_x + planex * camera;
+	ray.diry = mlx->player.dir_y + planey * camera;
+	ray.deltax = sqrt((ray.diry * ray.diry) / (ray.dirx * ray.dirx) + 1);///*ray.dirx == 0 ? 0 : */fabs(1/ray.dirx);
+	ray.deltay = sqrt((ray.dirx * ray.dirx) / (ray.diry * ray.diry) + 1);///*ray.diry == 0 ? 0 : */fabs(1/ray.diry);
+	ray.stepx = (ray.dirx < 0 ? -1 : 1);
+	ray.stepy = (ray.diry < 0 ? -1 : 1);
+	ray.sidex = ray.dirx < 0 ? (playerx - mapx) * ray.deltax : (mapx + 1.0f - playerx) * ray.deltax; //(r->x < 0 ? p->x - t.mx : t.mx - p->x + 1) * t.dx;
+	ray.sidey = ray.diry < 0 ? (playery - mapy) * ray.deltay : (mapy + 1.0f - playery) * ray.deltay; //working on it(r->y < 0 ? p->y - t.my : t.my - p->y + 1) * t.dy;
+	int hit = 0;
+	int side;
+	printf("stepx %d stepy %d\n", ray.stepx, ray.stepy);
+	printf("mapx %f mapy %f\n", mapx, mapy);
+	printf("looking\n");
+	while (hit == 0)
+	{
+		if (ray.sidex < ray.sidey)
+		{
+			ray.sidex += ray.deltax;
+			mapx += ray.stepx;
+			side = 0;
+		}
+		else
+		{
+			ray.sidey += ray.deltax;
+			mapy += ray.stepy;
+			side = 1;
+		}
+		//Check if ray has hit a wall
+		//printf("checking %f %f\n", mapx, mapy);
+		//if (mapx < 0 || mapx > (double)mlx->map.width - 1 || mapy < 0 || mapy > (double)mlx->map.height - 1)
+		//	return ;
+		//printf("ayy\n");
+		hit = *(mlx->map.matrix + (mlx->map.width * (int)mapy + (int)mapx));
+		//printf("hit %d\n", hit);
+		if (hit != 0)
+		{
+			printf("hit! %d at %f %f\n", hit, mapx, mapy);
+		}
+		//if (mapx < 0 || mapx >= (double)mlx->map.width || mapy < 0 || mapy >= (double)mlx->map.height)
+		//	break ;
+	}
+	printf("player: x(%f) y(%f)\n", mlx->player.x, mlx->player.y);
+	printf("done looking\n");
+	double prepwalldist;
+	if (side == 0)
+		prepwalldist = (mapx - playerx + (1.0 - ray.stepx) / 2.0) / ray.dirx;
+	else
+		prepwalldist = (mapy - playery + (1.0 - ray.stepy) / 2.0) / ray.diry;
+	draw_column(col, mlx, prepwalldist, hit);
 }
 
 void						draw_it(t_mlx *mlx)
 {
-    t_ray		ray;
-    t_player	*player;
-	double		camera;
 
-	
 	for(int x = 0; x < WIN_WIDTH; x++)
     {
-		player = &mlx->player;
-		double playerx = player->x;
-		double playery = player->y;
-		printf("pdir x %f pdir y %f", mlx->player.dir_x, mlx->player.dir_y);
-		printf("testing map w %d h %d f_w %f f_h %f\n", mlx->map.width, mlx->map.height, (double)mlx->map.width, (double)mlx->map.height);
-		double mapx = playerx;
-		double mapy = playery;
-
-
-    	double planex = player->camplane_x;
-		double planey = player->camplane_y;
-
-		camera = 2.0f * x / (double)WIN_WIDTH - 1.0f;
-		ray.dirx = mlx->player.dir_x + planex * camera;
-		ray.diry = mlx->player.dir_y + planey * camera;
-
-		ray.deltax = /*ray.dirx == 0 ? 0 : */fabs(1/ray.dirx);
-		ray.deltay = /*ray.diry == 0 ? 0 : */fabs(1/ray.diry);
-		ray.stepx = (ray.dirx < 0 ? -1 : 1);
-		ray.stepy = (ray.diry < 0 ? -1 : 1);
-		ray.sidex = ray.dirx < 0 ? (playerx - mapx) * ray.deltax : (mapx + 1.0f - playerx) * ray.deltax; //(r->x < 0 ? p->x - t.mx : t.mx - p->x + 1) * t.dx;
-		ray.sidey = ray.diry < 0 ? (playery - mapy) * ray.deltay : (mapy + 1.0f - playery) * ray.deltay; //working on it(r->y < 0 ? p->y - t.my : t.my - p->y + 1) * t.dy;
-		int hit = 0;
-		int side;
-		printf("stepx %d stepy %d\n", ray.stepx, ray.stepy);
-		printf("mapx %f mapy %f\n", mapx, mapy);
-		printf("looking\n");
-		while (hit == 0)
-		{
-			if (ray.sidex < ray.sidey)
-			{
-				ray.sidex += ray.deltax;
-				mapx += ray.stepx;
-				side = 0;
-			}
-			else
-			{
-				ray.sidey += ray.deltax;
-				mapy += ray.stepy;
-				side = 1;
-			}
-			//Check if ray has hit a wall
-			//printf("checking %f %f\n", mapx, mapy);
-			if (mapx >= 0 && mapx < (double)mlx->map.width && mapy >= 0 && mapy < (double)mlx->map.height)
-			{
-				//printf("ayy\n");
-				hit = *(mlx->map.matrix + (mlx->map.width * (int)mapy + (int)mapx));
-				//printf("hit %d\n", hit);
-			}
-			if (hit != 0)
-			{
-				printf("hit! %d at %f %f\n", hit, mapx, mapy);
-			}
-			//if (mapx < 0 || mapx >= (double)mlx->map.width || mapy < 0 || mapy >= (double)mlx->map.height)
-			//	break ;
-		}
-		printf("player: x(%f) y(%f)\n", mlx->player.x, mlx->player.y);
-		printf("done looking\n");
-		double prepwalldist;
-		if (side == 0)
-			prepwalldist = (mapx - playerx + (1.0 - ray.stepx) / 2.0) / ray.dirx;
-		else
-			prepwalldist = (mapy - playery + (1.0 - ray.stepy) / 2.0) / ray.diry;
-		draw_column(x, mlx, prepwalldist, hit);
+		cast(x, mlx);
 	}
 }
 
